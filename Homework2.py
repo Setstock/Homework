df6548dd3323791b6c43b6999524039afe98a5af
Setstock <109642661+Setstock@users.noreply.github.com>
2022-07-25 00:59:13 +0700

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -1,19 +1,36 @@
-N=int(input('How many stricks in the pile:'))
+import random
+N = int(input("How many stick in the pile? : "))
 print("There are",N,"sticks in the pile.")
-name=str(input("What is yor name:"))
-count = 0
+name = str(input("What is your name? : "))
+i=0
 while (N != 0):
-  x=int(input(name+",how many sticks you will take (1 or 2):"))
-  if (x>2):
-    print("No you cannot take more than 2 sticks!")
-  elif (x<1):
-    print("No you cannot take more less than 1 stick!")
-  elif (x>N):
-    print("There are no enough sticks to take.")
-  else:
-    count += 1
-    N=N-x
-    if (N != 0): 
-      print("There are",N,"sticks in the pile.")
+    if (i%2 == 0):
+        x = int(input(str(name)+" , How many stick you will take? (1 or 2 ) : "))
+        if (x>2):
+            print("No you can't take more than 2 sticks!")
+        elif(x<1):
+            print("No you can't take less than 1 stick!")
+        elif(N-x < 0):
+            print("there aren't enough sticks to take")
+        else:
+            N = N-x
+            i += 1
+            if(N == 0):
+                print(name," take the last  stick.")
+                print("Computer Winnnnn",name,"So saddd")
+            elif(N):
+                print("There are",N,"Sticks in the pile.\n")
     else:
-       print("OK. There is no stick left in the pile. You spent",count,"times.")
+        com = random.randint(1, 2)
+        print("Conputer take : ",com)
+        if(N - com < 0 ):
+            print("There aren't enough sticks to take.")
+        else:
+            N=N-com
+            i += 1
+            if(N == 0):
+                print("Computer take the last stick.")
+                print(name,"Winnn Computer so sad")
+            else:
+                print("There are",N,"sticks in the pile.\n")
+        
